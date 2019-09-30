6fb151f36546647ebfa54baecc7c9908b893e35e
Karim Elsenousi <karimelsenousi@karims-air.home>
2019-09-30 19:30:38 +0100

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import parse from './parse';
+import parse from './html';
+import parseCss from './css';
 
 const tree = parse('<p class="para">hello</p>');
 
-console.log(tree);
+const cssTree = parseCss('h3 { margin: auto; color: #cc000000; } .main {color: blue;}');
+console.log(JSON.stringify(cssTree, null, 2));
